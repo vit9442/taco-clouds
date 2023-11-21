c90720cbca4e2341fa1568c889f8b70d3a50319b
Vitaly <svirsky9442@gmail.com>
2023-11-21 18:03:30 +0300

    Initial commit

---
+++ src/main/java/com/example/tacoclouds/TacoCloudsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacoclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacoclouds/TacoCloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacoclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
